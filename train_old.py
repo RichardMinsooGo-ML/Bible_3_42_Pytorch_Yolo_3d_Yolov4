ba9ccffe5df5db98d2c0796043aa4b23196c6c39
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2022-12-16 09:48:50 +0900

    Add files via upload

--- train_old.py
+++ train_old.py
@@ -1,8 +1,3 @@
-
-'''
-1. Import Libraries for Data Engineering, Model Engineering
-'''
-
 from terminaltables import AsciiTable
 
 import os, sys, time, datetime, argparse
@@ -23,34 +18,21 @@ from config.train_config import parse_train_configs
 import pickle
 
 def main():
-    
-    '''
-    2. Set Hyperparameters w/argparse.
-    '''
     # Get data configuration
     configs = parse_train_configs()
     
     # Hardware configurations 
     print(configs.device)
-    
-    '''
-    3. Data Engineering
-    '''
-    
-    '''
-    4. Build NN model
-    '''
-    
+
+    # Initiate model
     model = create_model(configs).to(configs.device)
     # model.print_network()
     
     # Get data configuration
     class_names = load_classes("dataset/classes.names")
     
-    '''
-    5. Load Pre-trained data
-    '''    
     print(configs.pretrained_path)
+    
     assert os.path.isfile(configs.pretrained_path), "No file at {}".format(configs.pretrained_path)
 
     # If specified we start from checkpoint
@@ -94,14 +76,8 @@ def main():
         idx_cnt += 1
     """
     
-    '''
-    6. Optimizer Define
-    '''
     optimizer = torch.optim.Adam(model.parameters())
-    
-    '''
-    7. Metrics Define
-    '''
+
     metrics = [
         "grid_size",
         "loss",
@@ -123,9 +99,7 @@ def main():
         "conf_noobj",
     ]
     
-    '''
-    8. learning rate scheduler
-    '''
+    # learning rate scheduler config
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
     
     # f = open('checkpoints/global_epoch.pckl', 'rb')
@@ -134,47 +108,24 @@ def main():
     
     global_epoch = 0
     
-    '''
-    9. DataLoader
-    '''
     train_dataloader, train_sampler = create_train_dataloader(configs)
     # train_dataloader = create_val_dataloader(configs)
-    
-    '''
-    10. Define Loss Fumction
-    Yolo Loss is caculated at train step
-    '''
-    
-    '''
-    12. Define Episode / each step process
-    '''
     
     max_mAP = 0.5
     start_time = time.time() 
     for epoch in range(0, configs.num_epochs, 1):
         
-        '''
-        11. Define train loop
-        '''
         num_iters_per_epoch = len(train_dataloader)
 
         print(num_iters_per_epoch)
-        
-        '''
-        Tr 1. Model switch to train mode
-        '''
+
+        # switch to train mode
         model.train()
         
         epoch_loss = 0
-        
-        '''
-        # For each step(batch)
-        '''
+        # Training        
         for batch_idx, batch_data in enumerate(tqdm.tqdm(train_dataloader)):
             """
-            Tr 2. Explore Train_dataloader
-            Below Code will be used at the detailed Data Engineering
-            
             # print(batch_data)
             
             print(batch_data[0])
@@ -200,13 +151,8 @@ def main():
             img = Image.fromarray(data)
             img.save('my_img.png')
             img.show()
-            
-            # End of exploration.
             """
             
-            '''
-            Tr 3. Transfer Data to GPU if available
-            '''
             # data_time.update(time.time() - start_time)
             _, imgs, targets = batch_data
             global_step = num_iters_per_epoch * epoch + batch_idx + 1
@@ -215,98 +161,40 @@ def main():
 
             targets = targets.to(configs.device, non_blocking=True)
             imgs = imgs.to(configs.device, non_blocking=True)
-            
-            '''
-            Tr 4. Forward Pass
-            '''
-            '''
-            Tr 5. Compute Loss
-            '''
+
             total_loss, outputs = model(imgs, targets)
             
             epoch_loss += float(total_loss.item())
-            
-            '''
-            Tr 6. compute gradient and perform backpropagation
-            '''            
+            # compute gradient and perform backpropagation
             total_loss.backward()
 
             if global_step % configs.gradient_accumulations:
-                
-                '''
-                Tr 7. Accumulates gradient before each step
-                '''
+                # Accumulates gradient before each step
                 optimizer.step()
-                
-                '''
-                Tr 8. Adjust learning rate if IMPL is using learning rate scheduler
-                '''
+                # Adjust learning rate
                 lr_scheduler.step()
 
-                '''
-                Tr 9. Clear Gradients (zero the parameter gradients)
-                '''
+                # zero the parameter gradients
                 optimizer.zero_grad()
 
             else:
                 reduced_loss = total_loss.data
-            
-            """
-            Tr 10. [Opt] Metric Table and Log progress 
-            at each YOLO layer will be handled with separate chapter.
-            """
-            if (batch_idx+1) % int(len(train_dataloader)/3) == 0:
-
-                log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % ((epoch+1), configs.num_epochs, (batch_idx+1), len(train_dataloader))
-
-                metric_table = [["Metrics", *[f"YOLO Layer {i}" for i in range(len(model.yolo_layers))]]]
-
-                # Log metrics at each YOLO layer
-                for i, metric in enumerate(metrics):
-                    formats = {m: "%.6f" for m in metrics}
-                    formats["grid_size"] = "%2d"
-                    formats["cls_acc"] = "%.2f%%"
-                    row_metrics = [formats[metric] % yolo.metrics.get(metric, 0) for yolo in model.yolo_layers]
-                    metric_table += [[metric, *row_metrics]]
-
-                    # Tensorboard logging
-                    tensorboard_log = []
-                    for j, yolo in enumerate(model.yolo_layers):
-                        for name, metric in yolo.metrics.items():
-                            if name != "grid_size":
-                                tensorboard_log += [(f"{name}_{j+1}", metric)]
-                    tensorboard_log += [("loss", total_loss.item())]
-                    # logger.list_of_scalars_summary(tensorboard_log, global_step)
-
-                log_str += AsciiTable(metric_table).table
-                log_str += f"\nTotal loss {total_loss.item()}"
-
-                # Determine approximate time left for epoch
-                epoch_batches_left = len(train_dataloader) - (batch_idx + 1)
-                time_left = datetime.timedelta(seconds=epoch_batches_left * (time.time() - start_time) / (batch_idx + 1))
-                log_str += f"\n---- ETA {time_left}"
-
-                print(log_str)
                 
-            """
-            # End of Metric Table and Log progress 
-            """
+            # ----------------
+            #   Log progress
+            # ----------------
+
+        
+        
             
         crnt_epoch_loss = epoch_loss/num_iters_per_epoch
         
-        """
-        Tr 11. [Opt] Save model 
-        """
         torch.save(model.state_dict(), configs.save_path)
         # global_epoch += 1
         
         # print("Global_epoch :",global_epoch, "Current epoch loss : {:1.5f}".format(crnt_epoch_loss),'Saved at {}'.format(configs.save_path))
         print("Current epoch loss : {:1.5f}".format(crnt_epoch_loss),'Saved at {}'.format(configs.save_path))
         
-    
-    '''
-    14. Mean Average Precision
-    '''
     # Evaulation
     print("\n---- Evaluating Model ----")
     val_dataloader = create_val_dataloader(configs)
@@ -328,15 +216,14 @@ def main():
     print(f"---- mAP {AP.mean()}")
 
     max_mAP = AP.mean()
-    
+    #-------------------------------------------------------------------------------------
     """
-    [Opt]. Save checkpoint
-     
+    # Save checkpoint
     if (epoch+1) % configs.checkpoint_freq == 0:
         torch.save(model.state_dict(), configs.save_path)
         print('save a checkpoint at {}'.format(configs.save_path))
     """
-
+            
 if __name__ == '__main__':
     main()
     
